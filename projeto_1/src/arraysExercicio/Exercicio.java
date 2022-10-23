a72d262d383159b2865a3d0b31f8117ada659812
Abehmstur <matheus.medeiros970@gmail.com>
2022-10-23 08:06:46 -0300

    Commit teste nova classe desafio

--- projeto_1/src/arraysExercicio/Exercicio.java
+++ projeto_1/src/arraysExercicio/Exercicio.java
@@ -6,8 +6,9 @@ import java.util.Scanner;
 public class Exercicio {
    public static void main(String[] args) {
      System.out.println("hello");
-       Scanner ler = new Scanner(System.in);
-       int a = ler.nextInt();
-       System.out.println(a);
+       try (Scanner ler = new Scanner(System.in)) {
+        int a = ler.nextInt();
+         System.out.println(a);
+      }
    }
 }
