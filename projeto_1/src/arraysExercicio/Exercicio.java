5755953c568de4ad2c4a5bf05108489dac8deee9
Abehmstur <matheus.medeiros970@gmail.com>
2022-10-23 07:53:06 -0300

    novo repositorio

---
+++ projeto_1/src/arraysExercicio/Exercicio.java
@@ -0,0 +1,13 @@
+package arraysExercicio;
+
+
+import java.util.Scanner;
+
+public class Exercicio {
+   public static void main(String[] args) {
+     System.out.println("hello");
+       Scanner ler = new Scanner(System.in);
+       int a = ler.nextInt();
+       System.out.println(a);
+   }
+}
